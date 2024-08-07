7b16d419490fd9f32533ab6baf8613b8bd64815c
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-08-07 18:45:46 -0300

    criado o menu de opções

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Bem vindo a lista de nomes!");
+
+//indicando as opções do menu 
+Console.WriteLine("Escolha: " +
+    "\n 1-Inserir nome" +
+    "\n 2-Inserir nome em ínice específico" +
+    "\n 3-Exibir lista de nomes" +
+    "\n 4-Exibir nomes em ordem alfabética" +
+    "\n 5-Sair");
+
+int opcao = int.Parse(Console.ReadLine());
+switch (opcao)
+{
+    case 1:
+        Console.WriteLine("Essa é o opção 1");
+        break;
+        case 2:
+        Console.WriteLine("essa é o opção 2");
+        break;
+        case 3:
+        Console.WriteLine("Essa é a opção 3");
+        break;
+        case 4:
+        Console.WriteLine("Essa é opção 4");
+        break;
+        case 5:
+        Console.WriteLine("Obrigado!");
+        break;
+    default:
+        Console.WriteLine("opção inválida, tente novamente!");
+        break;
+}
